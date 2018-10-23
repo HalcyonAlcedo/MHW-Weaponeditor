eeb9e9805cff44b756c81022ddc667d98e77b440
alcedo <alcedo@alcedo.site>
2018-10-24 00:17:50 +0800

    0.0.1

---
+++ src/renderer/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import axios from 'axios'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+Vue.use(Vuetify)
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
